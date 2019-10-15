ef69632792313ebd4611f988dfb4ca3d4a99970b
CheerTipsukon <tipsukon.cheer@mail.kmutt.ac.th>
2019-10-15 18:50:46 +0700

    [Init]inital commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'phaser';
+import GameScene from './scenes/GameScene';
+
+
+const config = {
+    // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+    type: Phaser.WEBGL,
+    pixelArt: true,
+    roundPixels: true,
+    parent: 'content',
+    width: 600,
+    height: 900,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: {y:0}
+        }
+    },
+    scene: [
+        GameScene
+    ]
+};
+
+const game = new Phaser.Game(config);
---
+++ src/scenes/GameScene.js
@@ -0,0 +1,20 @@
+class GameScene extends Phaser.Scene {
+    constructor(){
+        super({
+            key: 'GameScene'
+        })
+    }
+
+    prelaod(){
+
+    }
+
+    create(){
+
+    }
+
+    update(delta, time){
+        
+    }
+}
+export default GameScene
